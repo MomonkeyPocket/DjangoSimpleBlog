57c0df5f213eaacecad53a7cf50a9d39461a430f
azkatang <mog2to4aki@gmail.com>
2022-10-09 23:08:02 +0900

    add each page and Form & Comment

---
+++ README.md
@@ -0,0 +1,3 @@
+## BULMA
+
+https://bulma.io/
--- microblog/microblog/urls.py
+++ microblog/microblog/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import top
+from blog.views import top, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', top),
+    path('<slug:slug>/', post_detail, name='post_detail')
 ]
